741d20bca359b666197a21caf36b3511501654b8
techartmedia <techartmedia.info@gmail.com>
2026-06-29 13:55:55 +0530

    chore: refine system build configurations

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // 👈 This matches Tailwind v4 requirements
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
